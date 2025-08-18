c064769c2d30a4560850b6be298d9a70563818fe
farzane <farzanekazemi8517@gmail.com>
2025-08-18 18:55:10 +0330

    fix: accessibility issue

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const accordions = document.querySelectorAll('.accordion')
+const accordions = document.querySelectorAll(".accordion");
 
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => toggleAccordion(accordion));
+  accordion.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
 
-
-accordions.forEach(( accordion) => {
-   accordion.addEventListener('click', ()=>{
-       accordion.getAttribute('aria-expanded') === 'true' ?
-       accordion.setAttribute('aria-expanded', 'false') :
-       accordion.setAttribute('aria-expanded', 'true')
-       console.log(accordion);
-    })
+      toggleAccordion(accordion);
+    }
+  });
 });
+
+function toggleAccordion(accordion) {
+  accordion.getAttribute("aria-expanded") === "true"
+    ? accordion.setAttribute("aria-expanded", "false")
+    : accordion.setAttribute("aria-expanded", "true");
+}
